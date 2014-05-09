ec0e76e5fd5b49fb332b2883035a630576f017bf
Piotr Kowalski <piecioshka@gmail.com>
2014-05-10 00:30:59 +0200

    First version of application

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wstęp do kryptologii - algorytm szybkiego potęgowania modularnego
+
+Mini aplikacja pozwalająca obliczyć liczbę podniesioną do potęgi w arytmetyce modularnej.
+
+## Demo
+
+[http://jsninja.pl/wkr-algorytm-szybkiego-potegowania-modularrnego/][1]
+
+## License
+
+[The MIT License][2]
+
+
+[1]: http://jsninja.pl/wkr-algorytm-szybkiego-potegowania-modularrnego/
+[2]: http://piecioshka.mit-license.org/
---
+++ scripts/app.js
@@ -0,0 +1,161 @@
+/**
+ * @author Piotr Kowalski <piecioshka@gmail.com>
+ * @see http://jsninja.pl/wkr-rozszerzony-algorytm-euklidesa/
+ * @licence The MIT License {@link http://piecioshka.mit-license.org/}
+ */
+(function (root, factory) {
+    root.App = factory(root._);
+}(this, function (_) {
+    'use strict';
+
+    function App(options) {
+        this.settings = _.extend({}, options);
+        this.initialize();
+    }
+
+    App.prototype = {
+        initialize: function () {
+            var s = this.settings;
+
+            _.bindAll(this, '_enterHandler', '_submitHandler');
+
+            // after click submit do calculation
+            s.submit.on('click', this._submitHandler);
+
+            // print result on any input have been modify
+            s.first.on('keydown', this._enterHandler);
+            s.second.on('keydown', this._enterHandler);
+            s.third.on('keydown', this._enterHandler);
+
+            // set focus on first input
+            s.first.focus();
+        },
+
+        _enterHandler: function (e) {
+            // if `enter`
+            if (e.keyCode === 13) {
+                if (this._checkParams()) {
+                    this._printResult(this._calculate());
+                }
+            } else {
+                this._clearResult();
+            }
+        },
+
+        _submitHandler: function (e) {
+            if (this._checkParams()) {
+                this._printResult(this._calculate());
+            }
+            e.preventDefault();
+        },
+
+        _checkParams: function () {
+            var s = this.settings;
+            var a = s.first.val();
+            var b = s.second.val();
+            var c = s.third.val();
+
+            if (!a.length || !b.length || !c.length) {
+                alert('Please fill mandatory params');
+                return false;
+            }
+
+            return true;
+        },
+
+        _toBits: function (number) {
+            var bits = Number(number).toString(2);
+            return bits.split('');
+        },
+
+        _calculate: function () {
+            var s = this.settings;
+            var a = s.first.val();
+            var b = s.second.val();
+            var c = s.third.val();
+
+            var data = [['xi', 'ai', 'ki']];
+            var lsbBits = this._toBits(b).reverse();
+
+            var i = 0;
+            var xi = null;
+            var ai = null;
+            var ki = null;
+
+            _.each(lsbBits, function (bit, index) {
+                i++;
+
+                if (xi === null) {
+                    xi = 1;
+                } else {
+                    if (data[i - 1][2]) {
+                        xi = (data[i - 1][0] * data[i - 1][1] ) % c;
+                    } else {
+                        xi = data[i - 1][0] % c;
+                    }
+                }
+
+                if (ai === null) {
+                    ai = +a;
+                } else {
+                    ai = (data[index][1] * data[index][1]) % c
+                }
+
+                ki = +bit;
+
+                data.push([xi, ai, ki]);
+            });
+
+            i++;
+
+            if (data[i - 1][2]) {
+                xi = (data[i - 1][0] * data[i - 1][1] ) % c;
+            } else {
+                xi = data[i - 1][0] % c;
+            }
+
+            data.push([xi, '-', '-']);
+
+            return data;
+        },
+
+        _printResult: function (result) {
+            var $table = $('<table>').addClass('table table-bordered table-condensed');
+            var $thead = $('<thead>');
+            var $tbody = $('<tbody>');
+
+            // -- header
+
+            var $tr = $('<tr>').addClass('info');
+            _.each(result.shift(), function (cell) {
+                var $th = $('<th>').text(cell);
+                $tr.append($th);
+            });
+            $thead.append($tr);
+            $table.append($thead);
+
+            // --- body
+
+            _.each(result, function (rows, rowIndex) {
+                var $tr = $('<tr>').addClass('active');
+                _.each(rows, function (cell, cellIndex) {
+                    var $td = $('<td>').text(cell);
+                    if ((_.size(result) - 1) === rowIndex && cellIndex === 0) $td.addClass('danger');
+                    $tr.append($td);
+                });
+                $tbody.append($tr);
+            });
+
+            $table.append($tbody);
+
+            // show table
+            this.settings.result.removeClass('hidden').html($table);
+        },
+
+        _clearResult: function () {
+            this.settings.result.addClass('hidden');
+        }
+    };
+
+    return App;
+}));
